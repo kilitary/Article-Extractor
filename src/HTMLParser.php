44aa81689a6e5936d0a71f1b952973ed0f2b06e1
deconf(iGuRE) <deconf@ya.ru>
2020-11-04 06:02:04 +0300

    check
    hook

--- src/HTMLParser.php
+++ src/HTMLParser.php
@@ -114,6 +114,7 @@
 		{
 			$dom = new \DOMDocument();
 			libxml_use_internal_errors(true);
+			//fix
 			$dom->loadHTML(mb_convert_encoding($html, 'HTML-ENTITIES', 'UTF-8'));
 			$xpath = new \DOMXPath($dom);
 			
